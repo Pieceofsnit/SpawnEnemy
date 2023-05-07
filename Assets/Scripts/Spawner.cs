0022f65c62c432d4c5db2f45ad267f1188d07aa5
Pieceofsnit <102411114+Pieceofsnit@users.noreply.github.com>
2023-05-07 21:14:06 +0500

    Update class Spawner

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private GameObject[] _gangEnemies;
+    [SerializeField] private List<Transform> _spawnPoints;
+    private int _delay  = 2;
+
+    private void Start()
+    {
+        _spawnPoints = new List<Transform>(_spawnPoints);
+        StartCoroutine(SpawnEnemy());
+    }
+
+    private IEnumerator SpawnEnemy()
+    {
+        for(int i = 0; i < _gangEnemies.Length; i++)
+        {
+            var spawn = Random.Range(0, _spawnPoints.Count);
+            Instantiate(_gangEnemies[i], _spawnPoints[spawn].transform.position, Quaternion.identity);
+            _spawnPoints.RemoveAt(spawn);
+            yield return new WaitForSeconds(_delay );
+        }
+    }
+}
