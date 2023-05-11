fdb947865da63939a8e7cb1be5e412bbd75d2eba
Pieceofsnit <102411114+Pieceofsnit@users.noreply.github.com>
2023-05-11 20:41:25 +0500

    New class Enemy, Update Spawner

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -4,21 +4,23 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    [SerializeField] private GameObject[] _gangEnemies;
+    [SerializeField] private List<Enemy> _gangEnemies;
     [SerializeField] private List<Transform> _spawnPoints;
+
     private int _delay  = 2;
 
     private void Start()
     {
         _spawnPoints = new List<Transform>(_spawnPoints);
-        StartCoroutine(SpawnEnemy());
+        StartCoroutine(SpawnEnemies());
     }
 
-    private IEnumerator SpawnEnemy()
+    private IEnumerator SpawnEnemies()
     {
-        for(int i = 0; i < _gangEnemies.Length; i++)
+        var spawn = Random.Range(0, _spawnPoints.Count -1);
+
+        for (int i = 0; i < _gangEnemies.Count; i++)
         {
-            var spawn = Random.Range(0, _spawnPoints.Count);
             Instantiate(_gangEnemies[i], _spawnPoints[spawn].transform.position, Quaternion.identity);
             _spawnPoints.RemoveAt(spawn);
             yield return new WaitForSeconds(_delay );
